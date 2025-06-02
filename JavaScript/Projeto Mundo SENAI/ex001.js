dde6f4890115bbd84ade1de5b1597ac3eaa4b513
Matheus Vicente <vicentematheus1507@gmail.com>
2025-06-02 15:58:17 -0300

    feat: alteração na lógica do projeto do mundo senai

--- JavaScript/Projeto Mundo SENAI/ex001.js
+++ JavaScript/Projeto Mundo SENAI/ex001.js
@@ -1,19 +1,37 @@
-let resistencia, tensao
+let resistencia, tensao, corrente
 
-function calcularCorrente(){
-    if(document.getElementById("resistencia").value != ""
-    && document.getElementById("tensao").value != ""){
+function resultado(){
+    if(document.getElementById("corrente").value == ""){
         resistencia = document.getElementById("resistencia").value
         tensao = document.getElementById("tensao").value
-        document.getElementById("resultadoCorrente").innerHTML += `${tensao / resistencia} A (amperes)`
+        document.getElementById("resultado").innerHTML = `A corrente é igual a ${tensao / resistencia} A (amperes)`
         document.getElementById("resistencia").value = ""
         document.getElementById("tensao").value = ""
+        document.getElementById("corrente").value = ""
+    }else if(document.getElementById("tensao").value == ""){
+        resistencia = document.getElementById("resistencia").value
+        corrente = document.getElementById("corrente").value
+        document.getElementById("resultado").innerHTML = `A tensão é igual a ${resistencia * corrente} V (volts)`
+        document.getElementById("resistencia").value = ""
+        document.getElementById("tensao").value = ""
+        document.getElementById("corrente").value = ""
+    }else if(document.getElementById("resistencia").value == ""){
+        tensao = document.getElementById("tensao").value
+        corrente = document.getElementById("corrente").value
+        document.getElementById("resultado").innerHTML = `A resistência é igual a ${tensao / corrente} Ω (ohms)`
+        document.getElementById("resistencia").value = ""
+        document.getElementById("tensao").value = ""
+        document.getElementById("corrente").value = ""
+    }else if(document.getElementById("corrente").value != ""
+    && document.getElementById("tensao").value != ""
+    && document.getElementById("resistencia").value != ""){
+        alert("Digite apenas dois valores realizar o cálculo!")
     }else{
-        alert("Digite a resistência e a tensão para calcular a corrente!")
+        alert("ERRO! Digite ao menos dois valores para realizar o cálculo")
     }
 }
 function apagar(){
-    document.getElementById("resultadoCorrente").innerHTML = "A corrente é igual a: "
+    document.getElementById("resultado").innerHTML = "Resultado: "
 }
 function mudaCor(){
     document.getElementById("bt1").style.backgroundColor = "gray"
