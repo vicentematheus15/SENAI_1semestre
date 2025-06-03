d9a60c13554017a929ae8351058166071802d39f
Mathues Vicente <vicentematheus1507@gmail.com>
2025-06-02 21:50:49 -0300

    feat: lista de exercicios funcoes professor dennis

---
+++ JavaScript/06 - Exercícios professor Dennis/index.html/ex01.js
@@ -0,0 +1,6 @@
+n1 = Number(prompt("Digite o primeiro número: "))
+n2 = Number(prompt("Digite o segundo número: "))
+function soma(n1,n2){
+    return n1+n2
+}
+console.log(soma(n1,n2))
---
+++ JavaScript/06 - Exercícios professor Dennis/index.html/ex02.js
@@ -0,0 +1,15 @@
+function maiorString(nomes){
+    let nome = ""
+    for(i = 0; i < nomes.length ; i++){ //faz o loop enquanto o i for menor que a quantidade de itens (nesse exemplo é 5)
+        var nome_temp = nomes[i]
+        if(nome_temp.length > nome.length){ //se o nome for maior que o nome anterior
+            nome = nome_temp //guarda ele na variavel nome
+        }
+        else{
+            continue //se nao for maior, nao guarda o nome em lugar nenhum e continua executando o loop
+        }
+    }
+    return nome //retorna o nome que ficou guardado na variavel nome, no caso, o maior
+}
+var nome = maiorString(["dennis", "carlos", "joao souza", "andré", "karen"])
+console.log(nome) // imprime o maior nome
