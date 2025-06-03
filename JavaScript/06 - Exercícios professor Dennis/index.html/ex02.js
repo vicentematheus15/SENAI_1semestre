3d25419291bdf3ca87cf5646ee7f065891c6357d
Matheus Vicente <vicentematheus1507@gmail.com>
2025-06-03 17:01:57 -0300

    feat exercicios lista dennis

--- JavaScript/06 - Exercícios professor Dennis/index.html/ex02.js
+++ JavaScript/06 - Exercícios professor Dennis/index.html/ex02.js
@@ -1,12 +1,9 @@
 function maiorString(nomes){
     let nome = ""
-    for(i = 0; i < nomes.length ; i++){ //faz o loop enquanto o i for menor que a quantidade de itens (nesse exemplo é 5)
+    for(let i = 0; i < nomes.length ; i++){ //faz o loop enquanto o i for menor que a quantidade de itens (nesse exemplo é 5)
         var nome_temp = nomes[i]
         if(nome_temp.length > nome.length){ //se o nome for maior que o nome anterior
             nome = nome_temp //guarda ele na variavel nome
-        }
-        else{
-            continue //se nao for maior, nao guarda o nome em lugar nenhum e continua executando o loop
         }
     }
     return nome //retorna o nome que ficou guardado na variavel nome, no caso, o maior
