d5efa5b669b8052d72f3021be761b93f8c578548
Matheus Vicente <vicentematheus1507@gmail.com>
2025-06-04 11:31:56 -0300

    feat: enhance calculation logic and input validation for Ohm's Law calculator

--- JavaScript/Projeto Mundo SENAI/script.js
+++ JavaScript/Projeto Mundo SENAI/script.js
@@ -1,41 +1,84 @@
-let resistencia, tensao, corrente
+let + resistencia, +tensao, +corrente;
 
-function resultado(){
-    if(document.getElementById("corrente").value == ""){
-        resistencia = document.getElementById("resistencia").value
-        tensao = document.getElementById("tensao").value
-        document.getElementById("resultado").innerHTML = `A corrente é igual a ${tensao / resistencia} A (amperes)`
-        document.getElementById("resistencia").value = ""
-        document.getElementById("tensao").value = ""
-        document.getElementById("corrente").value = ""
-    }else if(document.getElementById("tensao").value == ""){
-        resistencia = document.getElementById("resistencia").value
-        corrente = document.getElementById("corrente").value
-        document.getElementById("resultado").innerHTML = `A tensão é igual a ${resistencia * corrente} V (volts)`
-        document.getElementById("resistencia").value = ""
-        document.getElementById("tensao").value = ""
-        document.getElementById("corrente").value = ""
-    }else if(document.getElementById("resistencia").value == ""){
-        tensao = document.getElementById("tensao").value
-        corrente = document.getElementById("corrente").value
-        document.getElementById("resultado").innerHTML = `A resistência é igual a ${tensao / corrente} Ω (ohms)`
-        document.getElementById("resistencia").value = ""
-        document.getElementById("tensao").value = ""
-        document.getElementById("corrente").value = ""
-    }else if(document.getElementById("corrente").value != ""
-    && document.getElementById("tensao").value != ""
-    && document.getElementById("resistencia").value != ""){
-        alert("Digite apenas dois valores realizar o cálculo!")
-    }else{
-        alert("ERRO! Digite ao menos dois valores para realizar o cálculo")
-    }
+function resultado() {
+  resistencia = document.getElementById("resistencia").value;
+  tensao = document.getElementById("tensao").value;
+  corrente = document.getElementById("corrente").value;
+
+  //validacao  
+  if (
+    document.getElementById("corrente").value == "" &&
+    document.getElementById("tensao").value == "" &&
+    document.getElementById("resistencia").value == ""
+  ) {
+    alert("ERRO! Digite no mínimo dois valores para realizar o cálculo");
+  } else if (
+    document.getElementById("corrente").value == "" &&
+    document.getElementById("tensao").value == ""
+  ) {
+    alert("ERRO! Digite no mínimo dois valores para realizar o cálculo");
+  } else if (
+    document.getElementById("corrente").value == "" &&
+    document.getElementById("resistencia").value == ""
+  ) {
+    alert("ERRO! Digite no mínimo dois valores para realizar o cálculo");
+  } else if (
+    document.getElementById("tensao").value == "" &&
+    document.getElementById("resistencia").value == ""
+  ) {
+    alert("ERRO! Digite no mínimo dois valores para realizar o cálculo");
+  }
+
+  //calculo corrente
+  if (document.getElementById("corrente").value == "") {
+    document.getElementById("resultado").innerHTML = `Corrente = ${tensao / resistencia} A (amperes)`;
+    document.getElementById("resistencia").value = resistencia;
+    document.getElementById("tensao").value = tensao;
+    document.getElementById("corrente").value = tensao / resistencia;
+  }
+
+  //calculo tensao
+  else if (document.getElementById("tensao").value == "") {
+    document.getElementById("resultado").innerHTML = `Tensão = ${resistencia * corrente} V (volts)`;
+    document.getElementById("resistencia").value = resistencia;
+    document.getElementById("tensao").value = resistencia * corrente;
+    document.getElementById("corrente").value = corrente;
+  }
+
+  //calculo resistencia
+  else if (document.getElementById("resistencia").value == "") {
+    document.getElementById("resultado").innerHTML = `Resistência = ${tensao / corrente} Ω (ohms)`;
+    document.getElementById("resistencia").value = tensao / corrente;
+    document.getElementById("tensao").value = tensao;
+    document.getElementById("corrente").value = corrente;
+  } 
+  
+  //validacao
+  else if (
+    document.getElementById("corrente").value != "" &&
+    document.getElementById("tensao").value != "" &&
+    document.getElementById("resistencia").value != ""
+  ) {
+    alert("Digite apenas dois valores realizar o cálculo!");
+  } else {
+    alert("ERRO! Digite no mínimo dois valores para realizar o cálculo");
+  }
 }
-function apagar(){
-    document.getElementById("resultado").innerHTML = "Resultado: "
+function apagar() {
+  document.getElementById("resultado").innerHTML = "Resultado: ";
+  document.getElementById("corrente").value = "";
+  document.getElementById("tensao").value = "";
+  document.getElementById("resistencia").value = "";
 }
-function mudaCor(){
-    document.getElementById("bt1").style.backgroundColor = "gray"
+function mudaCorBt1() {
+  document.getElementById("bt1").style.backgroundColor = "gray";
 }
-function voltaCor(){
-    document.getElementById("bt1").style.backgroundColor = "white"
+function voltaCorBt1() {
+  document.getElementById("bt1").style.backgroundColor = "white";
+}
+function mudaCorBt2() {
+  document.getElementById("bt2").style.backgroundColor = "gray";
+}
+function voltaCorBt2() {
+  document.getElementById("bt2").style.backgroundColor = "white";
 }
