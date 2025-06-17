ca0828dcfbeb88524ceb8fac7c8c1bc9ad3d0504
Matheus Vicente <vicentematheus1507@gmail.com>
2025-06-17 19:54:14 -0300

    feat: add initial HTML structure and JavaScript functionality for student data submission

---
+++ Lógica de Programação/JavaScript/DOM/script.js
@@ -0,0 +1,9 @@
+let matricula, aluno
+
+function enviarDados(){
+    matricula = document.getElementById("idMatricula").value
+    aluno = document.querySelector("#idAluno").value
+    alert(`${matricula} - ${aluno}`)
+    document.getElementById("idMatricula").value = ""
+    document.querySelector("#idAluno").value = ""
+}
