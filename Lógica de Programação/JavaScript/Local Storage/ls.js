191e06fe14c8ee15858fd4538202ffe8d57b478d
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-01 20:32:24 -0300

    feat: fix typo in telefone field and add success alert after form submission

--- Lógica de Programação/JavaScript/Local Storage/ls.js
+++ Lógica de Programação/JavaScript/Local Storage/ls.js
@@ -5,10 +5,16 @@ function getDados(){
     nome = document.getElementById("nome").value
     sobrenome = document.getElementById("sobrenome").value
     email = document.getElementById("email").value
-    telefone = document.getElementById("teleofne").value
+    telefone = document.getElementById("telefone").value
     localStorage.setItem("cpf", cpf)
     localStorage.setItem("nome", nome)
     localStorage.setItem("sobrenome", sobrenome)
     localStorage.setItem("email", email)
     localStorage.setItem("telefone", telefone)
+    alert("Dados enviados com sucesso!")
+    document.getElementById("cpf").value = ""
+    document.getElementById("nome").value = ""
+    document.getElementById("sobrenome").value = ""
+    document.getElementById("email").value = ""
+    document.getElementById("telefone").value = ""
 }
