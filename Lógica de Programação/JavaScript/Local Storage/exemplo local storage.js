4397c0cfdf2ed2e4abe7e7feaa45e5115ed6d5ca
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-01 19:55:24 -0300

    feat: add example JavaScript code for local storage usage

---
+++ Lógica de Programação/JavaScript/Local Storage/exemplo local storage.js
@@ -0,0 +1,13 @@
+// localStorage.setItem("id", "3215")
+// localStorage.setItem("nome", "Maria")
+// localStorage.setItem("sobrenome", "Souza")
+// localStorage.setItem("email", "maria@gmail.com")
+
+// let nome = localStorage.getItem("nome")
+// let sobrenome = localStorage.getItem("sobrenome")
+
+// alert(`${nome} ${sobrenome}`)
+
+// localStorage.removeItem("sobrenome")  remove o sobrenome
+
+// localStorage.clear() //remove todos os dados
