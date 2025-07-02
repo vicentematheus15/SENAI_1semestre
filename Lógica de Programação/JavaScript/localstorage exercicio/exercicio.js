70a312b79e221d51adccbf425b51b031d0a78f54
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-01 22:01:10 -0300

    refactor: improve code formatting and consistency in local storage handling function

--- Lógica de Programação/JavaScript/localstorage exercicio/exercicio.js
+++ Lógica de Programação/JavaScript/localstorage exercicio/exercicio.js
@@ -1,18 +1,19 @@
-let nome = [], nota = []
+let nome = [],
+  nota = [];
 
-function getDados(){
-    if(localStorage.nomeLS){
-        nome = JSON.parse(localStorage.getItem("nomeLS"))
-    }
-    nome.push(document.getElementById("nome").value)
-    localStorage.nomeLS = JSON.stringify(nome)
+function getDados() {
+  if (localStorage.nomeLS) {
+    nome = JSON.parse(localStorage.getItem("nomeLS"));
+  }
+  nome.push(document.getElementById("nome").value);
+  localStorage.nomeLS = JSON.stringify(nome);
 
-    if(localStorage.notaLS){
-        cpf = JSON.parse(localStorage.getItem("notaLS"))
-    }
-    nota.push(document.getElementById("nota").value)
-    localStorage.notaLS = JSON.stringify(nota)
+  if (localStorage.notaLS) {
+    cpf = JSON.parse(localStorage.getItem("notaLS"));
+  }
+  nota.push(document.getElementById("nota").value);
+  localStorage.notaLS = JSON.stringify(nota);
 
-    document.getElementById("nome").value = ""
-    document.getElementById("nota").value = ""
+  document.getElementById("nome").value = "";
+  document.getElementById("nota").value = "";
 }
