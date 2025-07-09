0576d75e55210e1068cf96f380833a24ad9b1570
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-09 20:42:36 -0300

    feat: add script demonstrating the use of ternary operator for age verification

---
+++ Lógica de Programação/JavaScript/09/07/operadorTernario 2.js
@@ -0,0 +1,12 @@
+let idade = 28
+// let result
+// if(idade >= 18){
+//     result = "Maior de idade"
+// }else{
+//     result = "Menor de idade"
+// }
+// console.log(result)
+
+
+let result = idade >= 18 ? "Maior de idade" : "Menor de idade"
+console.log(result)
