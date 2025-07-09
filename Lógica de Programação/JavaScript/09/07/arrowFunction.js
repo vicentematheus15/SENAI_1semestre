328a8d94ae01769be46dfe8068d7dd4a2a7d2e75
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-09 20:42:13 -0300

    feat: add example of anonymous arrow function in JavaScript

---
+++ Lógica de Programação/JavaScript/09/07/arrowFunction.js
@@ -0,0 +1,6 @@
+//arrowFunction anônima
+//o que está antes da seta sao os parametros e depois da seta é o que vai retornar
+const result = (x, y) => x + y
+//passa os parametros que vao para a função
+console.log(result(5, 2))
+
