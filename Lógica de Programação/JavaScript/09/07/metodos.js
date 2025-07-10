0cba90849d4ae8446dbb6b07fad7735070e02821
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-09 21:20:14 -0300

    feat: add methods.js demonstrating array manipulation with map and filter

---
+++ Lógica de Programação/JavaScript/09/07/metodos.js
@@ -0,0 +1,31 @@
+const nums = [18, 14, 16, 11, 28, 22, 95, 13, 19, 64, 24]
+//let numsDobro = []
+
+//dobrar todos os numeros da array nums com FOR OF
+// for(n of nums){
+//     numsDobro.push(n * 2)
+// }
+// console.table(numsDobro)
+
+// MAP
+//retornar o dobro
+// const dobro = nums.map((x) => x * 2)
+// console.table(dobro)
+
+
+// FILTER
+//retornar numeros impares
+// const impares = nums.filter((x) => x % 2 != 0)
+// console.table(impares)
+
+//retornar maiores que 20                   o sorte ordena os numeros(porque todos tem 2 numeros)
+// const maior20 = nums.filter((x) => x > 20).sort()
+// console.table(maior20)
+
+//retornar multiplos de 2
+// const mult2 = nums.filter((x) => x % 2 == 0).sort((a, b) => b - a)
+// console.log(mult2)
+
+//retornar multiplos de 2
+// const mult2 = nums.filter((x) => x % 2 == 0).sort().reverse()
+// console.log(mult2)
