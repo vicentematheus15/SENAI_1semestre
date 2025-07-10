f192756529e1fa0e3029015a7284fe5e84f9be4d
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-09 21:30:52 -0300

    aula 09/07 sergio js

---
+++ Lógica de Programação/JavaScript/AULA JS SERGIO/objetos.js
@@ -0,0 +1,8 @@
+ let aluno = {
+    matricula : 32654,
+    nome : "MARIA",
+    sobrenome : "SANTOS",
+    email : "maria@gmail.com",
+    telefone : "48999154785"
+}
+console.log(`${aluno.nome} ${aluno.sobrenome}`)
