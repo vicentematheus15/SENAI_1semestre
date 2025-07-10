886ca6d40c3aa4f8659b655922141e8527771f03
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-09 21:43:01 -0300

    objetos e array de objetos

---
+++ Lógica de Programação/JavaScript/AULA JS SERGIO/array de objetos.js
@@ -0,0 +1,16 @@
+let aluno = [{
+    matricula : 32654,
+    nome : "MARIA",
+    sobrenome : "SANTOS",
+    email : "maria@gmail.com",
+    telefone : "48999154785",
+},
+{
+    matricula : 21556,
+    nome : "CARLOS",
+    sobrenome : "ANDRADE",
+    email : "carlos@gmail.com",
+    telefone : "48985468723",
+}]
+
+console.log(`${aluno[1].nome} ${aluno[1].sobrenome}`)
--- Lógica de Programação/JavaScript/AULA JS SERGIO/objetos.js
+++ Lógica de Programação/JavaScript/AULA JS SERGIO/objetos.js
@@ -3,6 +3,16 @@
     nome : "MARIA",
     sobrenome : "SANTOS",
     email : "maria@gmail.com",
-    telefone : "48999154785"
+    telefone : "48999154785",
+    endereco : {
+        Rua: "15 de Novembro",
+        num : 480,
+        Bairro : "Capoeiras",
+        cidade : "Fpolis",
+        UF: "SC"
+    }
 }
-console.log(`${aluno.nome} ${aluno.sobrenome}`)
+
+
+console.log(`${aluno.nome} ${aluno.sobrenome}
+${aluno.endereco.Rua} ${aluno.endereco.num} ${aluno.endereco.Bairro} ${aluno.endereco.cidade} ${aluno.endereco.UF}`)
