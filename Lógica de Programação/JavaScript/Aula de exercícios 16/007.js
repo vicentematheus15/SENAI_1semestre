8ff2353a77ed99ecdd466d5a6b3c8437c8e1169e
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-17 19:49:33 -0300

    feat: add script to calculate the factorial of a user-input number

---
+++ Lógica de Programação/JavaScript/Aula de exercícios 16/007.js
@@ -0,0 +1,12 @@
+let num = Number(prompt("Digite um número para descobrir o seu fatorial:"))
+let fat
+
+for(let i = num; i > 1; i--){
+    if (i == num) {
+        fat = num
+    }else{
+        fat *= i
+    }
+
+}
+alert(`O fatorial de ${num} é igual a ${fat}`)
