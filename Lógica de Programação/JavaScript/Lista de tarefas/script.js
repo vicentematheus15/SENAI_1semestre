dcd679abe70a30b6c39d5fca7246a61ba4254db4
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-28 19:53:12 -0300

    feat: populate task list with initial items in JavaScript

--- Lógica de Programação/JavaScript/Lista de tarefas/script.js
+++ Lógica de Programação/JavaScript/Lista de tarefas/script.js
@@ -1,3 +1,18 @@
 let input = document.querySelector('input[name=tarefa]')
 let btn = document.querySelector('#botao')
 let lista = document.querySelector("#lista")
+
+let tarefas = [
+    "Jogar videogame",
+    "Estudar JavaScript",
+    "Assistir um filme",
+    "Estudar Python",
+    "Ler um livro"   
+]
+
+
+{/* <li class="list-group-item list-group-item-action">Jogar videogame</li>
+                            <li class="list-group-item list-group-item-action">Estudar JavaScript</li>
+                            <li class="list-group-item list-group-item-action">Assistir um filme</li>
+                            <li class="list-group-item list-group-item-action">Estudar Python</li>
+                            <li class="list-group-item list-group-item-action">Ler um livro</li> */}
