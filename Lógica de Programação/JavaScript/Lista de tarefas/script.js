67622f629df8170834c4104c1aa328e3d86e4dc0
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-28 20:01:34 -0300

    feat: implement task rendering function to display tasks in the list

--- Lógica de Programação/JavaScript/Lista de tarefas/script.js
+++ Lógica de Programação/JavaScript/Lista de tarefas/script.js
@@ -11,8 +11,19 @@ let tarefas = [
 ]
 
 
-{/* <li class="list-group-item list-group-item-action">Jogar videogame</li>
+/* <li class="list-group-item list-group-item-action">Jogar videogame</li>
                             <li class="list-group-item list-group-item-action">Estudar JavaScript</li>
                             <li class="list-group-item list-group-item-action">Assistir um filme</li>
                             <li class="list-group-item list-group-item-action">Estudar Python</li>
-                            <li class="list-group-item list-group-item-action">Ler um livro</li> */}
+                            <li class="list-group-item list-group-item-action">Ler um livro</li> */
+
+function renderizarTarefas(){
+    for(let tarefa of tarefas){
+        let itemLista = document.createElement('li')
+        itemLista.setAttribute('class', 'list-group-item list-group-item-action')
+        let itemTexto = document.createTextNode(tarefa)
+        itemLista.appendChild(itemTexto)
+        lista.appendChild(itemLista)
+    }
+}
+renderizarTarefas()
