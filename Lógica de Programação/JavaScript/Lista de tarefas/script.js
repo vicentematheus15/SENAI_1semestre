8897e270e17e3f0e966cd13d7de28175cea66ed9
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-28 20:48:40 -0300

    feat: enhance task addition with validation and alert messages

--- Lógica de Programação/JavaScript/Lista de tarefas/script.js
+++ Lógica de Programação/JavaScript/Lista de tarefas/script.js
@@ -1,6 +1,7 @@
 let input = document.querySelector('input[name=tarefa]')
 let btn = document.querySelector('#botao')
 let lista = document.querySelector("#lista")
+let card = document.querySelector(".card")
 
 let tarefas = [
     "Jogar videogame",
@@ -22,7 +23,25 @@ function renderizarTarefas(){
 renderizarTarefas()
 
 btn.onclick = function(){
+    // tarefas = [""]
     let novaTarefa = input.value
-    tarefas.push(novaTarefa)
-    renderizarTarefas()
+    removeSpans()
+    if(novaTarefa != ""){
+        input.value = ""
+        tarefas.push(novaTarefa)
+        renderizarTarefas()
+    }else{
+        let span = document.createElement('span')
+        span.setAttribute('class', 'alert alert-warning')
+        let msg = document.createTextNode("Você precisa informar uma tarefa")
+        span.appendChild(msg)
+        card.appendChild(span)
+    }
+}
+
+function removeSpans(){
+    let spans = document.querySelectorAll('span')
+    for(let i = 0; i < spans.length; i++){
+        card.removeChild(spans[i])
+    }
 }
