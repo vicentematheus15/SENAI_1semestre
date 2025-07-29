484e9d223366c74df27e9968ccce846cafc34cc7
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-29 20:57:22 -0300

    feat: add methods to Pessoa and Carro classes for speaking and accelerating

--- Lógica de Programação/JavaScript/POO.js/class1.js
+++ Lógica de Programação/JavaScript/POO.js/class1.js
@@ -8,22 +8,35 @@ class Pessoa{
         this.nome = nome
         this.sobrenome = sobrenome
     }
+    falar(msg){
+        console.log(`${this.nome} está falando: ${msg}`)
+    }
+    comer(comida){
+        console.log(`${this.nomeCompleto()} está comendo ${comida}`)
+    }
     nomeCompleto(){
         return `${this.nome} ${this.sobrenome}`
     }
 }
+
+let p1 = new Pessoa("Pedro", "de Souza")
+// console.log(p1.nomeCompleto())
+let p2 = new Pessoa("Ana Paula", "Althoff")
+// console.log(p2.nomeCompleto())
+
+p2.comer("caldo")
+
 
 class Carro{
     constructor(modelo){
         this.modelo = modelo
         this.marca = 'Honda'
     }
+    acelerar(){
+        console.log(`${this.marca} ${this.modelo} está acelerando...`)
+    }
 }
 
-let p1 = new Pessoa("Pedro", "de Souza")
-console.log(p1.nomeCompleto())
-let p2 = new Pessoa("Ana Paula", "Althoff")
-console.log(p2.nomeCompleto())
-
 let c1 = new Carro("Civic")
-console.log(`${c1.marca} ${c1.modelo}`)
+c1.acelerar()
+// console.log(`${c1.marca} ${c1.modelo}`)
