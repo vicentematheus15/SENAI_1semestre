e4e2bab61ab75b4b3e344c5a93ac4f9678e7e4eb
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-29 20:37:00 -0300

    feat: add initial class definitions for Pessoa and Carro

---
+++ Lógica de Programação/JavaScript/POO.js/class1.js
@@ -0,0 +1,19 @@
+// class NomeClass{
+    //Atributos (características)
+    //Métodos (comportamentos)
+// }
+
+class Pessoa{
+    constructor(nome, sobrenome){
+        this.nome = nome
+        this.sobrenome = sobrenome
+    }
+}
+
+class Carro{
+    constructor(modelo){
+        this.modelo = modelo
+        this.marca = 'Honda'
+    }
+}
+
