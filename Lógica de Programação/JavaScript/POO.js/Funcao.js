fdd6fa0f3686ccb0b92a2a8652318ef52d2b596a
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-30 20:38:47 -0300

    feat: add Funcao class with constructor, getters/setters for cargo and salario

---
+++ Lógica de Programação/JavaScript/POO.js/Funcao.js
@@ -0,0 +1,19 @@
+class Funcao{
+    constructor(cargo, salario){
+        this._cargo = cargo
+        this._salario = salario
+    }
+    get cargo(){
+        return this._cargo
+    }
+    set cargo(cargo){
+        this._scargo= cargo
+    }
+    get salario(){
+        return this._salario
+    }
+    set salario(salario){
+        this._salario = salario
+    }
+}
+module.exports = Funcao
