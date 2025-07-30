3425e22c8275c96a7f9401c6520f9993747c1359
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-29 21:28:30 -0300

    feat: add getters and setters for Animal class properties

--- Lógica de Programação/JavaScript/POO.js/class2.js
+++ Lógica de Programação/JavaScript/POO.js/class2.js
@@ -1,5 +1,32 @@
 class Animal{
     constructor(reproducao, patas, locomocao, alimentacao){
-        
+        this._reproducao = reproducao
+        this._patas = patas
+        this._locomocao = locomocao
+        this._alimentacao = alimentacao
+    }
+    get reproducao(){
+        return this._reproducao
+    }
+    set reproducao(reproducao){
+        this._reproducao = reproducao
+    }
+    get patas(){
+        return this._patas
+    }
+    set patas(patas){
+        this._patas = patas
+    }
+    get locomocao(){
+        return this._locomocao
+    }
+    set locomocao(locomocao){
+        this._locomocao = locomocao
+    }
+    get alimentacao(){
+        return this._alimentacao
+    }
+    set alimentacao(alimentacao){
+        this._alimentacao = alimentacao
     }
 }
