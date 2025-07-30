a55f58dc69d7ec0bf2f23c2fe14cb9181486cc4f
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-30 20:27:53 -0300

    feat: implement Aluno class extending Pessoa with constructor and getters/setters

---
+++ Lógica de Programação/JavaScript/POO.js/Aluno.js
@@ -0,0 +1,31 @@
+const Pessoa = require('./Pessoa')
+class Aluno extends Pessoa{
+    constructor(nome, sobrenome, CPF, email, matricula, curso, idade){
+        super(nome, sobrenome, CPF, email)
+        this._matricula = matricula
+        this._curso = curso
+        this._idade = idade
+    }
+    get matricula(){
+        return this._matricula
+    }
+    set matricula(matricula){
+        this._matricula = matricula
+    }
+    get curso(){
+        return this._curso
+    }
+    set curso(curso){
+        this._curso = curso
+    }
+    get idade(){
+        return this._idade
+    }
+    set idade(idade){
+        this._idade = idade
+    }
+}
+
+let al1 = new Aluno("Nicodemos", "de Souza", "12345678910", "nds@gmail.com", 45987, "Banco de dados, 18")
+
+module.exports = Aluno
