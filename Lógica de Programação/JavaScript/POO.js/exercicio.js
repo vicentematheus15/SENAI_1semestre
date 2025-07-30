defcd56bc940a92624452a3bf3e86bfef9a183e0
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-29 22:03:09 -0300

    feat: implement Pessoas, Alunos, and Professores classes with constructors and methods

---
+++ Lógica de Programação/JavaScript/POO.js/exercicio.js
@@ -0,0 +1,88 @@
+class Pessoas{
+    constructor(nome, sobrenome, CPF, email){
+        this._nome = nome
+        this._sobrenome = sobrenome
+        this._CPF = CPF
+        this._email = email
+    }
+    get nome(){
+        return this._nome
+    }
+    set nome(nome){
+        this._nome = nome
+    }
+    get sobrenome(){
+        return this._sobrenome
+    }
+    set sobrenome(sobrenome){
+        this._sobrenome = sobrenome
+    }
+    get CPF(){
+        return this._CPF
+    }
+    set CPF(CPF){
+        this._CPF = CPF
+    }
+    get email(){
+        return this._email
+    }
+    set email(email){
+        this._email = email
+    }
+}
+
+class Alunos extends Pessoas{
+    constructor(nome, sobrenome, CPF, email, matricula, curso, idade){
+        super(nome, sobrenome, CPF, email)
+        this._matricula = matricula
+        this._curso = curso
+        this._idade = idade
+    }
+    get matricula(){
+        return this._matricula
+    }
+    set matricula(matricula){
+        this._matricula = matricula
+    }
+    get curso(){
+        return this._curso
+    }
+    set curso(curso){
+        this._curso = curso
+    }
+    get idade(){
+        return this._idade
+    }
+    set idade(idade){
+        this._idade = idade
+    }
+}
+
+class Professores extends Pessoas{
+    constructor(nome, sobrenome, CPF, email, area_de_formacao, salario){
+        super(nome, sobrenome, CPF, email)
+        this._area_de_formacao = area_de_formacao
+        this._salario = salario
+    }
+    get area_de_formacao(){
+        return this._area_de_formacao
+    }
+    set area_de_formacao(area_de_formacao){
+        this._area_de_formacao = area_de_formacao
+    }
+    get salario(){
+        return this._salario
+    }
+    set salario(salario){
+        this._salario = salario
+    }
+
+    recebeAumento(salario){
+        return salario + (salario * 0.1)
+    }
+    calculaGanhoAnual(){
+        return (salario * 12)
+    }
+}
+let prof1 = new Professores("Marcos", "Santos", "123.456.789-23", "marcos@gmail.com", "Desenvolvimento de Sistemas", 5000)
+console.table(prof1)
