39826e1b4d6475fb09a6a09e3a5d90523b3ade04
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-31 20:15:41 -0300

    feat: add limite_credito property with getter and setter in Linha_de_credito class

--- Lógica de Programação/JavaScript/Atividade Poo/Linha_de_credito.js
+++ Lógica de Programação/JavaScript/Atividade Poo/Linha_de_credito.js
@@ -1,7 +1,13 @@
 const Conta_bancaria = require('./Conta_bancaria')
 class Linha_de_credito extends Conta_bancaria{
-    constructor(tipo_conta, nome, sobrenome, CPF, saldo, ){
-        super(tipo_conta, nome, sobrenome, CPF, saldo, limite_credito)
+    constructor(tipo_conta, nome, sobrenome, CPF, saldo, limite_credito){
+        super(tipo_conta, nome, sobrenome, CPF, saldo)
+        this._limite_credito = limite_credito
+    }
+    get limite_credito(){
+        return this._limite_credito
+    }
+    set limite_credito(limite_credito){
         this._limite_credito = limite_credito
     }
 }
