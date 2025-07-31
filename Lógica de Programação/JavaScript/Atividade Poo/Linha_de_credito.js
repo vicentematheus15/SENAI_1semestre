00e977ce52fdc933720810244428dd899461667c
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-31 20:11:08 -0300

    feat: implement Linha_de_credito class extending Conta_bancaria with credit limit

---
+++ Lógica de Programação/JavaScript/Atividade Poo/Linha_de_credito.js
@@ -0,0 +1,8 @@
+const Conta_bancaria = require('./Conta_bancaria')
+class Linha_de_credito extends Conta_bancaria{
+    constructor(tipo_conta, nome, sobrenome, CPF, saldo, ){
+        super(tipo_conta, nome, sobrenome, CPF, saldo, limite_credito)
+        this._limite_credito = limite_credito
+    }
+}
+module.exports = Linha_de_credito
