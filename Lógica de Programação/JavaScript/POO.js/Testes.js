af2a4718ee0e4402327e1901415bf30a41319307
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-30 21:20:34 -0300

    feat: update Testes.js to include Aluno and demonstrate imprimirDados method for both Funcionario and Pessoa

--- Lógica de Programação/JavaScript/POO.js/Testes.js
+++ Lógica de Programação/JavaScript/POO.js/Testes.js
@@ -1,7 +1,13 @@
 const Funcionario = require('./Funcionario')
 const Funcao = require('./Funcao')
+const Pessoa = require('./Pessoa')
+const Aluno = require('./Aluno')
 
 let prof = new Funcao("Professor", 4275,22)
 let func1 = new Funcionario("Sandro", "Silva", "45789565621", "ss@Blob.com.br", prof)
-// console.table(func1)
+let alu1 = new Aluno("Enzo", "Stallone", "65478954923", "enzo@gmail.com", 10235, "Des. Sistemas", 18)
+//cada console.log está pegando a função imprimirDados de seu respectivo pai
+    //func1 herda de Funcionário
 console.log(func1.imprimirDados)
+    //alu1 herda de Pessoa
+console.log(alu1.imprimirDados())
