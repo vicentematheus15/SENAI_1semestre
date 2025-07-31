3c50969f10478ad1f8e9f00ae404bcc8ec35e731
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-31 20:43:46 -0300

    feat: add test instance for Conta_corrente class in Testes.js

--- Lógica de Programação/JavaScript/Atividade Poo/Testes.js
+++ Lógica de Programação/JavaScript/Atividade Poo/Testes.js
@@ -2,3 +2,6 @@ const Conta_bancaria = require('./Conta_bancaria')
 const Conta_corrente = require('./conta_corrente')
 const Conta_poupanca = require('./conta_poupanca')
 const Linha_de_credito = require('./linha_de_credito')
+
+let cliente1 = new Conta_corrente("Caio", "Hubert", "32154572654", "Corrente", "00001", 1000)
+// console.table(cliente1)
