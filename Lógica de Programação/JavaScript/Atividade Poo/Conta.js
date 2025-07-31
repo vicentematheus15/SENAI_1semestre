3beb0bb51ded12b414abe1073ddd0fd04cbcf155
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-31 19:45:54 -0300

    feat: implement Conta class with getters and setters for account properties

---
+++ Lógica de Programação/JavaScript/Atividade Poo/Conta.js
@@ -0,0 +1,33 @@
+class Conta{
+    constructor(numero_conta, nome, sobrenome, CPF){
+        this._numero_conta = numero_conta
+        this._nome = nome
+        this._sobrenome = sobrenome
+        this._CPF = CPF
+    }
+    get numero_conta(){
+        return this._numero_conta
+    }
+    set numero_conta(numero_conta){
+        this._numero_conta = numero_conta
+    }
+    get nome(){
+        return this._nome
+    }
+    set nome(nome){
+        this._nome = nome
+    }
+    get sobrenome(){
+        return this._sobrenome
+    }
+    set sobrenome(sobrenome){
+        this._sobrenome = sobrenome
+    }
+    get CPF(){
+        return this._CPF
+    }
+    set CPF(CPF){
+        this._CPF = CPF
+    }
+}
+MediaSourceHandle.exports = Conta
