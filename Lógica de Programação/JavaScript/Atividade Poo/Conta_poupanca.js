da8dcbb0cde81fd915c0f156db82481eb78a12bd
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-31 20:15:34 -0300

    feat: add getter and setter for rendimento in Conta_poupanca class

--- Lógica de Programação/JavaScript/Atividade Poo/Conta_poupanca.js
+++ Lógica de Programação/JavaScript/Atividade Poo/Conta_poupanca.js
@@ -4,5 +4,11 @@ class Conta_poupanca extends Conta_bancaria{
         super(nome, sobrenome, CPF, tipo_conta, numero_conta, saldo)
         this._rendimento = rendimento
     }
+    get rendimento(){
+        return this._rendimento
+    }
+    set rendimento(rendimento){
+        this._rendimento = rendimento
+    }
 }
 module.exports = Conta_poupanca
