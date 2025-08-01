67748ea3c86790269439bc9d51c9fa80a183b771
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-31 21:30:57 -0300

    feat: add saque method to Conta_corrente class for withdrawal management

--- Lógica de Programação/JavaScript/Atividade Poo/Conta_corrente.js
+++ Lógica de Programação/JavaScript/Atividade Poo/Conta_corrente.js
@@ -6,9 +6,17 @@ class Conta_corrente extends Conta_bancaria{
     imprimirDados(){  
         return `${this._nome} ${this._sobrenome}\n${this._CPF}\n${this._tipo_conta}\n${this._numero_conta}\n${this._saldo}`
     }
-    deposito(saldoAnterior, valorDepositado){
-        let novoSaldo = saldoAnterior + valorDepositado
-        return `Saldo anterior: R$${saldoAnterior.toFixed(2)}\nValor depositado: R$${valorDepositado.toFixed(2)}\nSaldo atual: R$${novoSaldo.toFixed(2)}`
+    deposito(saldoAtual, valorDepositado){
+        let novoSaldo = saldoAtual + valorDepositado
+        return `Saldo anterior: R$${saldoAtual.toFixed(2)}\nValor depositado: R$${valorDepositado.toFixed(2)}\nSaldo atual: R$${novoSaldo.toFixed(2)}`
+    }
+    saque(saldoAtual, valorSaque){
+        if(saldoAtual >= valorSaque){
+            let novoSaldo = saldoAtual - valorSaque
+            return `Saldo anterior: R$${saldoAtual.toFixed(2)}\nValor saque: R$${valorSaque.toFixed(2)}\nSaldo atual: R$${novoSaldo.toFixed(2)}`
+        }else{
+            return `Saldo insuficiente!`
+        }
     }
 }
 module.exports = Conta_corrente
