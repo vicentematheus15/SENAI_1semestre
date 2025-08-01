30a6913dd956cb6101c517e9a920542df0d9925c
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-31 21:21:42 -0300

    feat: add deposito method to Conta_corrente class for balance management

--- Lógica de Programação/JavaScript/Atividade Poo/Conta_corrente.js
+++ Lógica de Programação/JavaScript/Atividade Poo/Conta_corrente.js
@@ -6,5 +6,9 @@ class Conta_corrente extends Conta_bancaria{
     imprimirDados(){  
         return `${this._nome} ${this._sobrenome}\n${this._CPF}\n${this._tipo_conta}\n${this._numero_conta}\n${this._saldo}`
     }
+    deposito(saldoAnterior, valorDepositado){
+        let novoSaldo = saldoAnterior + valorDepositado
+        return `Saldo anterior: R$${saldoAnterior.toFixed(2)}\nValor depositado: R$${valorDepositado.toFixed(2)}\nSaldo atual: R$${novoSaldo.toFixed(2)}`
+    }
 }
 module.exports = Conta_corrente
