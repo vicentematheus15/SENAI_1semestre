399f84c677ecdd92bb6e3768279cb4a19a034cb3
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-31 21:48:23 -0300

    feat: add rendimentoMensal method to Conta_poupanca class for monthly interest calculation

--- Lógica de Programação/JavaScript/Atividade Poo/Conta_poupanca.js
+++ Lógica de Programação/JavaScript/Atividade Poo/Conta_poupanca.js
@@ -13,5 +13,10 @@ class Conta_poupanca extends Conta_bancaria{
     imprimirDados(){  
         return `${this._nome} ${this._sobrenome}\n${this._CPF}\n${this._tipo_conta}\n${this._numero_conta}\n${this._saldo}\n${this._porcentagem_rendimento}`
     }
+
+    rendimentoMensal(saldoAtual, porcentagemRendimento){
+        let novoSaldo = saldoAtual + (saldoAtual*porcentagemRendimento/100)
+        return `O seu saldo atual é de R$${saldoAtual.toFixed(2)} e está rendendo ${porcentagemRendimento}% ao mês.\nNo próximo mês o seu saldo será de R$${novoSaldo.toFixed(2)}`
+    }
 }
 module.exports = Conta_poupanca
