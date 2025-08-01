255ba8212601bc638d0016d4bdc29f49527e4d83
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-31 21:06:35 -0300

    feat: update test cases to use imprimirDados method and add new test for Linha_de_credito

--- Lógica de Programação/JavaScript/Atividade Poo/Testes.js
+++ Lógica de Programação/JavaScript/Atividade Poo/Testes.js
@@ -3,9 +3,14 @@ const Conta_corrente = require('./conta_corrente')
 const Conta_poupanca = require('./conta_poupanca')
 const Linha_de_credito = require('./linha_de_credito')
 
+//TESTES CONTA CORRENTE
 let cliente1 = new Conta_corrente("Caio", "Souza", "11111111111", "Corrente", "00001", 2000)
-console.table(cliente1.dadosPessoais())
+console.table(cliente1.imprimirDados())
 
+//TESTES CONTA POUPANÇA
 let cliente2 = new Conta_poupanca("Carla", "Rodrigues", "22222222222", "Poupança", "00002", 1000, 5)
+console.table(cliente2.imprimirDados())
 
-let cliente3 = new Linha_de_credito("Pedro", "Rocha", "33333333333", "Linha de crédito", "00003", 0, 4000)
+//TESTES LINHA DE CREDITO
+let cliente3 = new Linha_de_credito("Pedro", "Rocha", "33333333333", "Linha de crédito", 4000, 0)
+console.table(cliente3.imprimirDados())
