b20543c5c92b8e377dcbca8aa2bf758378d79392
Matheus Vicente <vicentematheus1507@gmail.com>
2025-07-31 21:53:19 -0300

    feat: add valor_fatura property to Linha_de_credito class for enhanced credit line management

--- Lógica de Programação/JavaScript/Atividade Poo/Linha_de_credito.js
+++ Lógica de Programação/JavaScript/Atividade Poo/Linha_de_credito.js
@@ -1,8 +1,9 @@
 const Conta_bancaria = require('./Conta_bancaria')
 class Linha_de_credito extends Conta_bancaria{
-    constructor(nome, sobrenome, CPF, tipo_conta, limite_credito, saldo){
+    constructor(nome, sobrenome, CPF, tipo_conta, limite_credito, valor_fatura, saldo){
         super(nome, sobrenome, CPF, tipo_conta, limite_credito, saldo)
         this._limite_credito = limite_credito
+        this._valor_fatura = valor_fatura
     }
     get limite_credito(){
         return this._limite_credito
@@ -10,8 +11,14 @@ class Linha_de_credito extends Conta_bancaria{
     set limite_credito(limite_credito){
         this._limite_credito = limite_credito
     }
+    get valor_fatura(){
+        return this._valor_fatura
+    }
+    set valor_fatura(valor_fatura){
+        this._valor_fatura = valor_fatura
+    }
     imprimirDados(){  
-        return `${this._nome} ${this._sobrenome}\n${this._CPF}\n${this._tipo_conta}\n${this._limite_credito}\n${this._saldo}`
+        return `${this._nome} ${this._sobrenome}\n${this._CPF}\n${this._tipo_conta}\n${this._limite_credito}\n${this.valor_fatura}\n${this._saldo}`
     }
 }
 module.exports = Linha_de_credito
