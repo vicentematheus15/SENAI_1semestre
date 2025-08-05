6df6badc601c631ef587864244a5704076f6e976
Matheus Vicente <vicentematheus1507@gmail.com>
2025-08-04 21:39:53 -0300

    feat: add quantity input and footer information to product display in ecommerce project

--- Lógica de Programação/Projeto ecommerce/js/script.js
+++ Lógica de Programação/Projeto ecommerce/js/script.js
@@ -1,5 +1,5 @@
 let login = '', senha, qtdCont = 0, valor = 0, totalGeral = 0, cesta, loginAut
-let article, div, div2, h3, p1, span, aLink, main, section, footer, h2, p3, span2,aLink2
+let article, div, div2, h3, p1, span, aLink, main, section, footer, h2, p3, span2,aLink2, input
 let usr = []
 let snh = []
 
@@ -54,5 +54,52 @@ function montaHTML(){
         h3.setAttribute('id','nome' + i)
         h3.innerHTML = produto[i]
         article.append(h3)
+        p1 = document.createElement('p')
+        p1.innerHTML = 'Qtd: '
+        p1.setAttribute('hidden', 'true')
+        input = document.createElement('input')
+        input.setAttribute('type', 'number')
+        input.setAttribute('value', '1')
+        input.setAttribute('min', '1')
+        input.setAttribute('max', '10')
+        input.setAttribute('hidden', 'true')
+        input.setAttribute('id', 'qtd-' + i)
+        p1.append(input)
+        article.append(p1)
+        p2 = document.createElement('p')
+        p2.innerHTML = 'R$ '
+        span = document.createElement('span')
+        span.setAttribute('id', cod[i])
+        span.setAttribute('class', 'bold')
+        span.innerHTML = preco[i].toFixed(2).replace('.', ',')
+        p2.append(span)
+        article.append(p2)
+        // aLink = document.createElement('a')
+        // aLink.setAttribute('onclick', 'compra(' + "'" + 'qtd-' + i + "'" + ',' + "'" + cod[i] + "'" + ',' + i + ")")
+    }
+//cria o footer da pagina
+    footer = document.createElement('footer')
+    footer.setAttribute('id', 'rodape')
+    document.body.append(footer)
+    h2 = document.createElement('h2')
+    h2.innerHtml = 'Informações sobre o site'
+    footer.append(h2)
+    p3 = document.createElement('p')
+    p3.innerHTML = '&copy; 2025'
+    footer.append(p3)
+    span2 = document.createElement('span')
+    span2.setAttribute('class', 'bold')
+    span2.innerHTML = 'Loja dos Nerds'
+    aLink2 = document.createElement('a')
+    aLink2.setAttribute('id', 'adm')
+    aLink2.setAttribute('href', 'atualizacao.html')
+    aLink2.innerHTML = 'Administração'
+    footer.append(aLink2)
+    p3.append(span2)
+    let logA = localStorage.getItem('loginAutenticado')
+    if(logA == 'null' || logA == 'undefined'){
+        document.getElementById('log').innerHTML = 'login'
+    }else{
+        document.getElementById('log').innerHTML = `Bem-vindo, ${localStorage.getItem('loginAutenticado')}`
     }
 }
